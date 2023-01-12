89b9f0b5066805966dbd37193cc542f7c4d9f9c3
Manik Dingra <manikdhingra0582@gmail.com>
2023-01-12 23:49:52 +0530

    n

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,10 @@ import "./Header.css"
 function Header() {
     return (
         <div className='header'>
+            <img src="/images/logo.svg" alt="" className='logo' />
+            <div className="menu">
 
+            </div>
         </div>
     )
 }
